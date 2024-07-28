79892d2d089404313dddb4e2b8c0b1e42bb9783f
Yuki <yuuki.walsh@outlook.com>
2024-07-28 23:51:21 +1000

    Create MultiOrVisibilityConverter.cs

---
+++ Converters/MultiOrVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿namespace WpfUtils.Converters;
+
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+public class MultiOrVisibilityConverter : IMultiValueConverter
+{
+	public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+	{
+		foreach (object value in values)
+		{
+			if (value is Visibility visValue)
+			{
+				if (visValue == Visibility.Visible)
+				{
+					return Visibility.Visible;
+				}
+			}
+		}
+
+		return Visibility.Collapsed;
+	}
+
+	public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+	{
+		throw new NotSupportedException("BooleanAndConverter is a OneWay converter.");
+	}
+}
