5935fb1041daf004f9190ba3f797853d91c9579d
Yuki <yuuki.walsh@outlook.com>
2024-08-12 19:19:50 +1000

    add task.Run()

---
+++ Extensions/TaskExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace WpfUtils.Extensions;
+
+using System;
+using System.Threading.Tasks;
+using WpfUtils.Logging;
+
+public static class TaskExtensions
+{
+	public static void Run(this Task self)
+	{
+		Task.Run(async () =>
+		{
+			try
+			{
+				await self;
+			}
+			catch(Exception ex)
+			{
+				Log.Error(ex, "Error in Task");
+			}
+		});
+	}
+}
