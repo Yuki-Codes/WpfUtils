2aa7ec956b7467e8cea2948db5bad6c50b4d6a08
Yuki <yuuki.walsh@outlook.com>
2024-08-20 01:38:46 +1000

    ListBoxEx to fix left mouse selection on down instead of up

---
+++ Controls/ListBoxEx.cs
@@ -0,0 +1,27 @@
+﻿namespace WpfUtils.Controls;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+public class ListBoxEx : ListBox
+{
+	protected override DependencyObject GetContainerForItemOverride()
+	{
+		return new ListBoxItemEx();
+	}
+}
+
+public class ListBoxItemEx : ListBoxItem
+{
+	protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+	{
+		// intentional don't call base so that selection happens on release
+	}
+
+	protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
+	{
+		// intentional flip so that selection happens on release
+		base.OnMouseLeftButtonDown(e);
+	}
+}
