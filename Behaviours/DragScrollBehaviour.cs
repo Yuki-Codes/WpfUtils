aa82ff74cfd1a0c7c17b15e299677c33ab0403eb
Yuki <yuuki.walsh@outlook.com>
2024-08-20 02:42:27 +1000

    drag scroll now has velocity

--- Behaviours/DragScrollBehaviour.cs
+++ Behaviours/DragScrollBehaviour.cs
@@ -1,6 +1,9 @@
 ﻿namespace WpfUtils.Behaviours;
 
 using DependencyPropertyGenerator;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -20,18 +23,22 @@ public static partial class ScrollViewerExtensions
 
 public class ScrollDragBehaviour
 {
+	private const double MaxVelocity = 50;
+
 	private readonly ScrollViewer scrollViewer;
 	private Point startDragPoint;
 	private double startDragHorizontalOffset;
 	private double startDragVerticalOffset;
 	private bool isDragging = false;
+	private Task? animationTask;
+	private bool isMouseDown = false;
 
 	public ScrollDragBehaviour(ScrollViewer scrollViewer)
 	{
 		this.scrollViewer = scrollViewer;
 
 		this.scrollViewer.PreviewMouseDown += this.OnPreviewMouseDown;
-		this.scrollViewer.MouseMove += this.OnMouseMove;
+		this.scrollViewer.PreviewMouseMove += this.OnMouseMove;
 		this.scrollViewer.MouseUp += this.OnMouseUp;
 	}
 
@@ -40,9 +47,8 @@ public class ScrollDragBehaviour
 		if (e.Handled)
 			return;
 
+		this.isMouseDown = true;
 		this.startDragPoint = e.GetPosition(this.scrollViewer);
-		this.startDragHorizontalOffset = this.scrollViewer.HorizontalOffset;
-		this.startDragVerticalOffset = this.scrollViewer.VerticalOffset;
 	}
 
 	private void OnMouseMove(object sender, MouseEventArgs e)
@@ -65,21 +71,35 @@ public class ScrollDragBehaviour
 		}
 
 		Point dragPoint = e.GetPosition(this.scrollViewer);
-		Vector delta = dragPoint - this.startDragPoint;
+		Vector totalDelta = dragPoint - this.startDragPoint;
 
-		if (delta.Length <= 10)
+		if (!this.isDragging && totalDelta.Length <= 5)
 			return;
 
-		this.isDragging = true;
-		this.scrollViewer.Cursor = Cursors.ScrollAll;
+		if (!this.isDragging)
+		{
+			this.startDragPoint = e.GetPosition(this.scrollViewer);
+			this.startDragHorizontalOffset = this.scrollViewer.HorizontalOffset;
+			this.startDragVerticalOffset = this.scrollViewer.VerticalOffset;
 
-		this.scrollViewer.ScrollToHorizontalOffset(this.startDragHorizontalOffset - delta.X);
-		this.scrollViewer.ScrollToVerticalOffset(this.startDragVerticalOffset - delta.Y);
+			if (this.scrollViewer.Content is FrameworkElement fe)
+				fe.IsHitTestVisible = false;
+
+			this.isDragging = true;
+			this.scrollViewer.Cursor = Cursors.ScrollAll;
+
+			this.Animate();
+		}
+
+		this.scrollViewer.ScrollToHorizontalOffset(this.startDragHorizontalOffset - totalDelta.X);
+		this.scrollViewer.ScrollToVerticalOffset(this.startDragVerticalOffset - totalDelta.Y);
 		e.Handled = true;
 	}
 
 	private void OnMouseUp(object sender, MouseEventArgs e)
 	{
+		this.isMouseDown = false;
+
 		if (e.Handled)
 			return;
 
@@ -89,7 +109,57 @@ public class ScrollDragBehaviour
 
 			e.Handled = true;
 			this.scrollViewer.Cursor = null;
-			this.scrollViewer.ReleaseMouseCapture();
+
+			if (this.scrollViewer.Content is FrameworkElement fe)
+			{
+				fe.IsHitTestVisible = true;
+			}
 		}
+	}
+
+	private void Animate()
+	{
+		if (this.animationTask == null || this.animationTask.IsCompleted)
+		{
+			this.animationTask = this.AnimateToTarget();
+		}
+	}
+
+	private async Task AnimateToTarget()
+	{
+		Vector velocity = new Vector(0, 0);
+		Point lastDragPoint = Mouse.GetPosition(this.scrollViewer);
+
+		// drag
+		do
+		{
+			Point dragPoint = Mouse.GetPosition(this.scrollViewer);
+			velocity = dragPoint - lastDragPoint;
+			lastDragPoint = dragPoint;
+
+			await Task.Delay(100);
+			await this.scrollViewer.Dispatcher.MainThread();
+		}
+		while (this.isMouseDown && this.isDragging);
+
+		// release
+		velocity *= 0.25;
+
+		// slide
+		do
+		{
+			velocity.X = Math.Clamp(velocity.X, -MaxVelocity, MaxVelocity);
+			velocity.Y = Math.Clamp(velocity.Y, -MaxVelocity, MaxVelocity);
+
+			this.scrollViewer.ScrollToHorizontalOffset(this.scrollViewer.HorizontalOffset - velocity.X);
+			this.scrollViewer.ScrollToVerticalOffset(this.scrollViewer.VerticalOffset - velocity.Y);
+
+			velocity.X /= 1.05;
+			velocity.Y /= 1.05;
+
+			await Task.Delay(15);
+			await this.scrollViewer.Dispatcher.MainThread();
+		}
+		while (velocity.Length > 0.01 && !this.isMouseDown);
 	}
 }
