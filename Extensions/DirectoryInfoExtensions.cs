64be84d53f53eed7fba8456111809b0ba0768a54
Yuki <yuuki.walsh@outlook.com>
2024-08-20 23:08:46 +1000

    directory extensions

---
+++ Extensions/DirectoryInfoExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace System.IO;
+
+using System;
+using System.Collections.Generic;
+
+public static class DirectoryInfoExtensions
+{
+	private static readonly Dictionary<string, Environment.SpecialFolder> SpecialFolderLookup = new();
+
+	static DirectoryInfoExtensions()
+	{
+		foreach (Environment.SpecialFolder folder in Enum.GetValues<Environment.SpecialFolder>())
+		{
+			string path = Environment.GetFolderPath(folder).ToLowerInvariant();
+
+			if (SpecialFolderLookup.ContainsKey(path))
+				continue;
+
+			SpecialFolderLookup.Add(path, folder);
+		}
+	}
+
+	public static bool IsSpecialFolder(this DirectoryInfo self)
+	{
+		return SpecialFolderLookup.ContainsKey(self.FullName.ToLowerInvariant());
+	}
+
+	public static Environment.SpecialFolder? GetSpecialFolder(this DirectoryInfo self)
+	{
+		Environment.SpecialFolder folder;
+		if (!SpecialFolderLookup.TryGetValue(self.FullName.ToLowerInvariant(), out folder))
+			return null;
+
+		return folder;
+	}
+}
---
+++ Extensions/SpecialFolderExtensions.cs
@@ -0,0 +1,41 @@
+﻿namespace System;
+
+using FontAwesome.Sharp;
+
+public static class SpecialFolderExtensions
+{
+	public static IconChar? ToIcon(this Environment.SpecialFolder self)
+	{
+		switch (self)
+		{
+			case Environment.SpecialFolder.Desktop: return IconChar.Desktop;
+			case Environment.SpecialFolder.Programs: return IconChar.WindowMaximize;
+			case Environment.SpecialFolder.MyDocuments: return IconChar.FileLines;
+			case Environment.SpecialFolder.Favorites: return IconChar.Star;
+			case Environment.SpecialFolder.MyMusic: return IconChar.Music;
+			case Environment.SpecialFolder.MyVideos: return IconChar.Film;
+			case Environment.SpecialFolder.DesktopDirectory: return IconChar.Desktop;
+			case Environment.SpecialFolder.MyComputer: return IconChar.Computer;
+			case Environment.SpecialFolder.NetworkShortcuts: return IconChar.NetworkWired;
+			case Environment.SpecialFolder.Fonts: return IconChar.Font;
+			case Environment.SpecialFolder.ApplicationData: return IconChar.Server;
+			case Environment.SpecialFolder.LocalApplicationData: return IconChar.Server;
+			case Environment.SpecialFolder.CommonApplicationData: return IconChar.Server;
+			case Environment.SpecialFolder.Windows: return IconChar.Windows;
+			case Environment.SpecialFolder.System: return IconChar.Windows;
+			case Environment.SpecialFolder.ProgramFiles: return IconChar.WindowMaximize;
+			case Environment.SpecialFolder.MyPictures: return IconChar.Images;
+			case Environment.SpecialFolder.UserProfile: return IconChar.User;
+			case Environment.SpecialFolder.SystemX86: return IconChar.Windows;
+			case Environment.SpecialFolder.ProgramFilesX86: return IconChar.WindowMaximize;
+			case Environment.SpecialFolder.CommonProgramFiles: return IconChar.WindowMaximize;
+			case Environment.SpecialFolder.CommonProgramFilesX86: return IconChar.WindowMaximize;
+			case Environment.SpecialFolder.CommonDocuments: return IconChar.File;
+			case Environment.SpecialFolder.CommonMusic: return IconChar.Music;
+			case Environment.SpecialFolder.CommonPictures: return IconChar.Images;
+			case Environment.SpecialFolder.CommonVideos: return IconChar.Film;
+		}
+
+		return null;
+	}
+}
