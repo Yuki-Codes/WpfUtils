bb6097bb6b493b4cd3f2dafb162ea211fd975297
Yuki <yuuki.walsh@outlook.com>
2024-08-24 02:27:46 +1000

    added RoutedEvent markup extension

---
+++ MarkupExtensions/RoutedEventExtension.cs
@@ -0,0 +1,46 @@
+﻿namespace WpfUtils.MarkupExtensions;
+
+using System;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Markup;
+
+public class RoutedEventExtension(string path)
+	: MarkupExtension
+{
+	public override object? ProvideValue(IServiceProvider serviceProvider)
+	{
+		int separatorIndex = path.LastIndexOf('.');
+		if (separatorIndex == -1)
+			throw new NotSupportedException("Invalid routed event path");
+
+		string typeName = path.Substring(0, separatorIndex);
+		string name = path.Substring(separatorIndex + 1);
+
+		IXamlTypeResolver? xamlTypeResolver = serviceProvider.GetService(typeof(IXamlTypeResolver)) as IXamlTypeResolver;
+		if (xamlTypeResolver == null)
+			throw new InvalidOperationException("Missing Xaml Type Resolver");
+
+		Type? type = xamlTypeResolver.Resolve(typeName);
+		if (type == null)
+			throw new InvalidOperationException($"Unable to locate type: '{typeName}'");
+
+		FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.Static);
+		foreach (FieldInfo field in fields)
+		{
+			if (field.FieldType == typeof(RoutedEvent))
+			{
+				RoutedEvent? evt = field.GetValue(null) as RoutedEvent;
+				if (evt == null)
+					continue;
+
+				if (evt.Name == name)
+				{
+					return evt;
+				}
+			}
+		}
+
+		throw new InvalidOperationException($"Unable to locate event: '{name}'");
+	}
+}
