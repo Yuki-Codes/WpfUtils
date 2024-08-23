045373d2e0b33385d292069dfe4107d675681ed2
Yuki <yuuki.walsh@outlook.com>
2024-08-23 18:01:58 +1000

    TextBoxEx with suffix

---
+++ Controls/TextBoxEx.cs
@@ -0,0 +1,46 @@
+﻿namespace WpfUtils.Controls;
+
+using DependencyPropertyGenerator;
+using System.Windows;
+using System.Windows.Controls;
+
+[DependencyProperty<object>("Suffix")]
+[DependencyProperty<object>("SuffixTemplate")]
+[DependencyProperty<Thickness>("SuffixPosition")]
+public partial class TextBoxEx : TextBox
+{
+	public override void OnApplyTemplate()
+	{
+		base.OnApplyTemplate();
+		this.UpdateSuffix();
+	}
+
+	protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+	{
+		base.OnRenderSizeChanged(sizeInfo);
+		this.UpdateSuffix();
+	}
+
+	protected override void OnTextChanged(TextChangedEventArgs e)
+	{
+		base.OnTextChanged(e);
+		this.UpdateSuffix();
+	}
+
+	partial void OnSuffixChanged()
+	{
+		this.UpdateSuffix();
+	}
+
+	private void UpdateSuffix()
+	{
+		Rect r = this.GetRectFromCharacterIndex(this.Text.Length, true);
+		if (r != Rect.Empty)
+		{
+			Thickness m = this.SuffixPosition;
+			m.Left = r.Left;
+			m.Top = r.Top;
+			this.SuffixPosition = m;
+		}
+	}
+}
---
+++ Converters/RectToThicknessConverter.cs
@@ -0,0 +1,16 @@
+﻿namespace WpfUtils.Converters;
+
+using System.Windows;
+
+public class RectToThicknessConverter : ConverterBase<Rect, Thickness>
+{
+	protected override Thickness Convert(Rect value)
+	{
+		return new Thickness(value.Left, value.Top, value.Right, value.Bottom);
+	}
+
+	protected override Rect ConvertBack(Thickness value)
+	{
+		return new Rect(value.Left, value.Top, value.Right, value.Bottom);
+	}
+}
