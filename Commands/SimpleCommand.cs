e0ac54017ba990194fb9b3f405b635ed7ef65918
Yuki <yuuki.walsh@outlook.com>
2024-08-25 21:44:18 +1000

    Update SimpleCommand.cs

--- Commands/SimpleCommand.cs
+++ Commands/SimpleCommand.cs
@@ -1,38 +1,113 @@
 ﻿namespace WpfUtils.Commands;
 
 using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Threading;
 
 public class SimpleCommand : ICommand
 {
-	private readonly Action action;
-	private readonly Func<bool> canExecute;
+	protected Action? action;
+	protected Func<Task>? asyncFunc;
+	protected Func<bool>? canExecute;
+	private bool isExecuting = false;
+	private EventHandler? canExecuteChanged;
+
+	public SimpleCommand()
+	{
+		CommandManager.RequerySuggested += this.OnRequerySuggested;
+	}
 
 	public SimpleCommand(Action action, Func<bool> canExecute)
+		: this()
 	{
 		this.action = action;
 		this.canExecute = canExecute;
 	}
 
 	public SimpleCommand(Action action)
+		: this()
 	{
 		this.action = action;
-		this.canExecute = () => true;
+	}
+
+	public SimpleCommand(Func<Task> func)
+		: this()
+	{
+		this.asyncFunc = func;
 	}
 
 	public event EventHandler? CanExecuteChanged
 	{
-		add { CommandManager.RequerySuggested += value; }
-		remove { CommandManager.RequerySuggested -= value; }
+		add
+		{
+			// yuck
+			Dispatcher currentDispatcher = Dispatcher.CurrentDispatcher;
+			this.canExecuteChanged += (s, e) =>
+			{
+				currentDispatcher?.BeginInvoke(() =>
+				{
+					value?.Invoke(s, e);
+				});
+			};
+		}
+
+		remove
+		{
+			this.canExecuteChanged -= value;
+		}
 	}
 
 	public bool CanExecute(object? parameter)
 	{
-		return this.canExecute.Invoke();
+		if (this.isExecuting)
+			return false;
+
+		return this.CanExecute();
 	}
 
 	public void Execute(object? parameter)
 	{
-		this.action.Invoke();
+		this.isExecuting = true;
+		this.RaiseCanExecuteChanged();
+
+		this.action?.Invoke();
+
+		Task.Run(async () =>
+		{
+			try
+			{
+				if (this.asyncFunc != null)
+					await this.asyncFunc.Invoke();
+
+				await this.Execute();
+			}
+			finally
+			{
+				this.isExecuting = false;
+				this.RaiseCanExecuteChanged();
+			}
+		});
+	}
+
+	protected void RaiseCanExecuteChanged()
+	{
+		this.canExecuteChanged?.Invoke(this, new());
+	}
+
+	protected virtual bool CanExecute()
+	{
+		return this.canExecute?.Invoke() ?? true;
+	}
+
+	protected virtual Task Execute()
+	{
+		return Task.CompletedTask;
+	}
+
+	private void OnRequerySuggested(object? sender, EventArgs e)
+	{
+		this.canExecuteChanged?.Invoke(sender, e);
 	}
 }
