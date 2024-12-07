244ead7dd5092a3503114c69fe7ce040c7e00e3d
Yuki <yuuki.walsh@outlook.com>
2024-12-07 18:39:23 +1100

    switch selector is now its own control

---
+++ Controls/SwitchSelector.cs
@@ -0,0 +1,11 @@
+﻿namespace WpfUtils.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+public class SwitchSelector : ListBox
+{
+}
