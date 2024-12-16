068c0df6d519a83e9b5b5f28da111bb9b553f247
Yuki <yuuki.walsh@outlook.com>
2024-12-16 22:50:54 +1100

    additional converters

---
+++ Converters/ColumnVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace WpfUtils.Converters;
+
+using System.Windows;
+
+public class ColumnVisibilityConverter : ConverterBase<bool, GridLength>
+{
+	protected override GridLength Convert(bool value)
+	{
+		GridLength length = GridLength.Auto;
+
+		if (this.Parameter != null)
+		{
+			GridLengthConverter lConverter = new();
+			object? paramLengthObj = lConverter.ConvertFrom(this.Parameter);
+
+			if (paramLengthObj is GridLength gl)
+				length = gl;
+		}
+
+		return value ? length : new GridLength(0);
+	}
+}
---
+++ Converters/MultiplyConverter.cs
@@ -0,0 +1,16 @@
+﻿namespace WpfUtils.Converters;
+
+public class MultiplyConverter : ConverterBase<double, double>
+{
+	protected override double Convert(double value)
+	{
+		double p = System.Convert.ToDouble(value);
+		return value * p;
+	}
+
+	protected override double ConvertBack(double value)
+	{
+		double p = System.Convert.ToDouble(value);
+		return value / p;
+	}
+}
