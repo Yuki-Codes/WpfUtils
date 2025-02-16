701dfc9c97d68c849b2be46dde32b0d50157b345
Yuki <Yuki.Walsh@Outlook.com>
2025-02-16 19:29:46 +1100

    EnumToIndex converter

---
+++ Converters/EnumToIndexConverter.cs
@@ -0,0 +1,28 @@
+﻿namespace WpfUtils.Converters;
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+[ValueConversion(typeof(Enum), typeof(int))]
+public class EnumToIndexConverter : IValueConverter
+{
+	public object? Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+	{
+		if (value == null)
+			return -1;
+
+		return (int)value;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		if (value is not int iVal)
+			return Binding.DoNothing;
+
+		if (!targetType.IsEnum)
+			throw new Exception("Enum converter can only be used on an enum type");
+
+		return Enum.ToObject(targetType, iVal);
+	}
+}
