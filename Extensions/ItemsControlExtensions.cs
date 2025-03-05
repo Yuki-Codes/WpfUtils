66cd07d0c1b282c896a9eca03a504b9d4055c53e
Yuki <Yuki.Walsh@Outlook.com>
2025-03-05 23:33:07 +1100

    ItemsControlExtensions

---
+++ Extensions/ItemsControlExtensions.cs
@@ -0,0 +1,54 @@
+﻿namespace WpfUtils.Extensions;
+
+using System.Windows;
+using System.Windows.Controls;
+
+public static class ItemsControlExtensions
+{
+	public static int GetVisibleItemsCount(this ItemsControl self)
+	{
+		Rect controlBounds = new Rect(0.0, 0.0, self.ActualWidth, self.ActualHeight);
+		int count = 0;
+
+		foreach (object? item in self.Items)
+		{
+			DependencyObject container = self.ItemContainerGenerator.ContainerFromItem(item);
+
+			if (container is FrameworkElement el && el.IsVisible)
+			{
+				Rect containerBounds = el.TransformToAncestor(self).TransformBounds(new Rect(0, 0, el.ActualWidth, el.ActualHeight));
+
+				if (controlBounds.Contains(containerBounds.TopLeft) && controlBounds.Contains(containerBounds.BottomRight))
+				{
+					count++;
+				}
+			}
+		}
+
+		return count;
+	}
+
+	public static void HideClippedItems(this ItemsControl self, long opacity = 0)
+	{
+		Rect controlBounds = new Rect(0.0, 0.0, self.ActualWidth, self.ActualHeight);
+
+		foreach (object? item in self.Items)
+		{
+			DependencyObject container = self.ItemContainerGenerator.ContainerFromItem(item);
+
+			if (container is FrameworkElement el && el.IsVisible)
+			{
+				Rect containerBounds = el.TransformToAncestor(self).TransformBounds(new Rect(0, 0, el.ActualWidth, el.ActualHeight));
+
+				if (controlBounds.Contains(containerBounds.TopLeft) != controlBounds.Contains(containerBounds.BottomRight))
+				{
+					el.Opacity = opacity;
+				}
+				else
+				{
+					el.Opacity = 1.0;
+				}
+			}
+		}
+	}
+}
